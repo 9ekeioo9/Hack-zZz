e651bbe711aa3dfd345b5834bf47d7922a130459
9ekeioo9 <9ekeioo9@gmail.com>
2024-07-10 18:48:25 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    API_ID = "27917193"
+    API_HASH = "bb368fbb6bd2c6ff589a1f1038de3046"
+    TOKEN = "7348915717:AAESLMsnfxROQRv8-X10yMRQOzu-tenJVlU"  
+    START_PIC = "https://telegra.ph/file/94b680ab579fa0d976c11.jpg"
+    CHAT = "-1002187310645"
